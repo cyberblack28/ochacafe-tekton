6f6ebeb1261bcf7cdfc770d8fb8da4f07dfccb7e
yutaka.ichikawa <yutaka.ichikawa@oracle.com>
2024-08-14 12:49:40 +0000

    latest

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "OCI & Red Hat OpenShift !!")
+        fmt.Fprintf(w,  "Welcome OCI & Red Hat OpenShift !!")
 }
 
 func main() {
